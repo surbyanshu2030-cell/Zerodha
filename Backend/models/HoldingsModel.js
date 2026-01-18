3d640e5752a0a85284e9b48e475d1f07a7bd4468
Surbhi Tyagi <surbyanshu2030@gmail.com>
2026-01-18 18:50:15 +0530

    Backend setup with MongoDB connection

--- Backend/models/HoldingsModel.js
+++ Backend/models/HoldingsModel.js
@@ -1,7 +1,7 @@
 const { model } = require('mongoose');
 
-const { HoldingSchema } = require("../schemas/HoldingSchema");
+const { HoldingSchema } = require("../schemas/HoldingsSchema");
 
 const HoldingsModel = new model("Holding", HoldingSchema);
 
-module.exports = HoldingsModel;
+module.exports = { HoldingsModel };
